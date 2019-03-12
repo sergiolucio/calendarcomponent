f659c45daa8f9dd2302657ab10288315cf4bf916
António Sérgio Sequeira Lúcio <lucio.antoniosergio@gmail.com>
2019-03-12 09:24:04 +0000

    + configuração inicial do projeto + layout da vista anual criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import {SidebarModule} from 'ng-sidebar';
+import { YearViewComponent } from './year-view/year-view.component';
+import { TestComponent } from './test/test.component';
+
+
+
+const appRoutes: Routes = [
+  { path: 'year-view', component: YearViewComponent}
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    YearViewComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    SidebarModule.forRoot()
+    SidebarModule.forRoot(),
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
